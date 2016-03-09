fb14429e5c78039488654b21f7966f1f37581ac1
Darkxell <darkxell.mc@gmail.com>
2016-03-09 21:37:01 +0100

    doc, gitignore

--- desktop/src/com/norwand/game/desktop/DesktopLauncher.java
+++ desktop/src/com/norwand/game/desktop/DesktopLauncher.java
@@ -5,12 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.norwand.game.MainGame;
 
+/** The main launcher for the desktop application. */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Les Chroniques de Norwand";
-		config.addIcon("assets/textures/gui/icon.png", FileType.Internal);
-		@SuppressWarnings("unused")
-		LwjglApplication app = new LwjglApplication(new MainGame("assets/"), config);
-	}
+
+    public static void main(String[] arg) {
+	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	config.title = "Les Chroniques de Norwand";
+	config.addIcon("assets/textures/gui/icon.png", FileType.Internal);
+	@SuppressWarnings("unused")
+	LwjglApplication app = new LwjglApplication(new MainGame("assets/"),
+		config);
+    }
+
 }
