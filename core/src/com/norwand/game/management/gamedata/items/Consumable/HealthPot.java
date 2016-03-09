978f7c3224767fcd6b225895a6abd1c24a4321d8
mroklom <valentin.marechaldu37@laposte.net>
2016-03-09 22:43:49 +0100

    Changed description of HealthPotion

--- core/src/com/norwand/game/management/gamedata/items/Consumable/HealthPot.java
+++ core/src/com/norwand/game/management/gamedata/items/Consumable/HealthPot.java
@@ -11,7 +11,7 @@ public class HealthPot extends Item {
 
     public HealthPot(){
         this.displayName = "Potion de vie";
-        this.itemDescription = "Potion de vie, permet de récuperer des coeurs.";
+        this.itemDescription = "Potion de vie, permet de récuperer ... de la vie.";
     }
 
     @Override
