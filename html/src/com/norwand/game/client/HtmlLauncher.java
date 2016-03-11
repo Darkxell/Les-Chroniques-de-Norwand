02914931ea48e1038d65d635cd6bec717e06710d
Darkxell <darkxell.mc@gmail.com>
2016-03-11 14:32:35 +0100

    html fix?

--- html/src/com/norwand/game/client/HtmlLauncher.java
+++ html/src/com/norwand/game/client/HtmlLauncher.java
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new MainGame("");
+                return new MainGame("../../desktop/assets/");
         }
 }
