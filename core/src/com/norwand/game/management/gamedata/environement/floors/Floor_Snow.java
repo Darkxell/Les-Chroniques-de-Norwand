606bcf48595bc3c713755fde2170ea6be7751e3a
AlexBuleon <alex.buleon@gmail.com>
2016-03-18 00:57:06 +0100

    Agrandissement de la map + suppression de DebugState

--- core/src/com/norwand/game/management/gamedata/environement/floors/Floor_Snow.java
+++ core/src/com/norwand/game/management/gamedata/environement/floors/Floor_Snow.java
@@ -4,15 +4,17 @@ import com.badlogic.gdx.Gdx;
 import com.norwand.game.MainGame;
 import com.norwand.game.management.gamedata.environement.Floor;
 import com.norwand.game.management.gamedata.environement.FloorData;
+import com.norwand.game.management.gamedata.environement.entities.monsters.Mimic;
 import com.norwand.game.management.gamedata.environement.tileentities.SignPost;
 import com.norwand.game.management.gamedata.player.Player;
 
 public class Floor_Snow extends Floor {
 
     public Floor_Snow(Player playerpointer) {
-	super(new FloorData(Gdx.files.internal(MainGame.ASSETSPATH
-		+ "maps/snow.txt")), playerpointer);
-	addTileEntity(new SignPost(this, 25, 27));
+        super(new FloorData(Gdx.files.internal(MainGame.ASSETSPATH
+            + "maps/snow.txt")), playerpointer);
+        addTileEntity(new SignPost(this, 25, 27));
+        addEntity(new Mimic(this, 21, 9.5));
     }
 
 }
