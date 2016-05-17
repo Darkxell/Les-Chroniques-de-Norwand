aec6e95e515e9e043d78b03c87a10b6191c111ee
V-ssk <v-ssk@hotmail.fr>
2016-05-17 17:46:07 +0200

    add monsters

---
+++ core/src/com/norwand/game/management/gamedata/environement/entities/monsters/IceMonster.java
@@ -0,0 +1,48 @@
+package com.norwand.game.management.gamedata.environement.entities.monsters;
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.norwand.game.management.gamedata.GameData;
+import com.norwand.game.management.gamedata.environement.Floor;
+import com.norwand.game.management.gamedata.environement.entities.Monster;
+import com.norwand.game.resources.ImagesHolder;
+import com.norwand.game.utility.objects.DoubleRectangle;
+
+/**
+ * Created by V-SSK on 17/05/2016.
+ */
+// add projectile for this monster ( snowballs)
+public class IceMonster extends Monster {
+    public IceMonster(Floor roompointer, double x, double y) {
+        super(roompointer, x, y);
+    }
+    private int spritecounter = 25;
+
+    @Override
+    public void update() {
+        --spritecounter;
+        if (spritecounter < 0)
+            spritecounter = 50;
+        if(knockback())
+            GameData.get().player.info.health -= 0.20;
+    }
+
+    @Override
+    public Pixmap getCurrentSprite() {
+        return ImagesHolder.entityset.getTile((spritecounter < 25) ? 725 : 733);
+    }
+
+    @Override
+    public void onAct() {
+
+    }
+
+    @Override
+    public DoubleRectangle getHitbox(double posX, double posY) {
+        return new DoubleRectangle(posX - 0.35, posY - 0.35, 0.7, 0.7);
+    }
+
+    @Override
+    public void onhit(double damage) {
+        kill();
+    }
+}
---
+++ core/src/com/norwand/game/management/gamedata/environement/entities/monsters/SnowBird.java
@@ -0,0 +1,57 @@
+package com.norwand.game.management.gamedata.environement.entities.monsters;
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.norwand.game.management.gamedata.GameData;
+import com.norwand.game.management.gamedata.environement.Floor;
+import com.norwand.game.management.gamedata.environement.entities.Monster;
+import com.norwand.game.resources.ImagesHolder;
+import com.norwand.game.utility.objects.DoubleRectangle;
+import com.norwand.game.utility.objects.MathVector;
+import com.norwand.game.utility.objects.Position;
+
+/**
+ * Created by V-SSK on 17/05/2016.
+ */
+public class SnowBird extends Monster {
+    public SnowBird(Floor roompointer, double x, double y) {
+        super(roompointer, x, y);
+    }
+    private int spritecounter = 50;
+    private MathVector direction;
+    @Override
+    public void update() {
+
+        direction = new MathVector(GameData.get().player.x - posX, GameData.get().player.y - posY);
+        if (direction.getBasicLength() < 4.5) {
+            Position temp = direction.getFixedTranslation(posX, posY, (spritecounter > 25) ? 0.02 : 0.08);
+            posX = temp.x;
+            posY = temp.y;
+        }
+        --spritecounter;
+        if (spritecounter < 0 )
+            spritecounter = 50;
+        if(knockback())
+            GameData.get().player.info.health -= 0.5;
+
+    }
+
+    @Override
+    public Pixmap getCurrentSprite() {
+        return ImagesHolder.entityset.getTile((spritecounter < 25) ? 100 : 108);
+    }
+
+    @Override
+    public void onAct() {
+
+    }
+
+    @Override
+    public DoubleRectangle getHitbox(double posX, double posY) {
+        return new DoubleRectangle(posX - 0.35, posY - 0.35, 0.7, 0.7);
+    }
+
+    @Override
+    public void onhit(double damage) {
+        kill();
+    }
+}
